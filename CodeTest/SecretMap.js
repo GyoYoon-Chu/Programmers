2d210f03199d57573cda6a10fb2af83074e90750
Chu gyo yoon <ksn03012@naver.com>
2021-02-04 22:58:52 +0900

    21-02-04

---
+++ CodeTest/SecretMap.js
@@ -0,0 +1,27 @@
+function solution(n, arr1, arr2) {
+    var resultList = [];
+
+    for (var i = 0; i < n; i++) {
+        var result = "";
+        var c = (arr1[i] | arr2[i]).toString(2);
+        if (c.length < n) {
+            var s = "";
+            for (var j = 0; j < n - c.length; j++) {
+                s += "0";
+            }
+            c = s + c;
+        }
+        for (var j = 0; j < n; j++) {
+            if (c[j] === '1') {
+                result += '#';
+            }
+            else {
+                result += ' ';
+            }
+        }
+        resultList[i] = result;
+    }
+    return resultList;
+}
+
+console.log(solution(6, [46, 33, 33, 22, 31, 50], [27, 56, 19, 14, 14, 10]));
